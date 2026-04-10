8b7b7746425eaffea2bf93147c0fc10be273598a
dishashukla24 <dishashukla0110@gmail.com>
2026-04-10 12:05:53 +0530

    ProductCatalogue service

--- ProductPrice/src/main/java/cg/example/ProductPrice/ProductPriceRepo.java
+++
@@ -1,9 +0,0 @@
-package cg.example.ProductPrice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface ProductPriceRepo extends JpaRepository<ProductPrice, Integer>{
-
-}
